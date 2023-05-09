834b477c378144c03f5e828539245795ab0cc01b
Alex Jones <alexjajones1@gmail.com>
2023-05-09 14:31:16 +0000

    futher tmux updates

---
+++ project_switcher.sh
@@ -0,0 +1,8 @@
+selected=$(ls ~/code | sed -E 's/:.*$//' | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse)
+selected_name=$(echo $selected | tr "." -)
+
+if [ "$selected_name" = "" ]; then
+    echo "Strings are equal."
+else
+	tmuxinator start $selected_name -n $selected_name -p ~/default.yml workspace="~/code/${selected}"
+fi
