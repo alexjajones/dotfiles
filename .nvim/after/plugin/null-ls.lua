13a6992361d5701059b011e246e936833c63ca13
Alex Jones <alexjajones1@gmail.com>
2023-11-18 13:21:36 +0000

    Update null-ls.lua

--- .nvim/after/plugin/null-ls.lua
+++ .nvim/after/plugin/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.completion.spell,
+        -- null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.black.with({
             args = { "--stdin-filename", "$FILENAME", "--line-length=100", "--quiet", "-" }
         })
